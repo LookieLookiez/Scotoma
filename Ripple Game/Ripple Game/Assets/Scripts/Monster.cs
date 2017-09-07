f616ba7637eb2feeca30f576f08405a410c01e88
Nicholas Duxbury <110317@student.sae.edu.au>
2017-09-07 17:29:03 +1000

    Changes to the monster, can now change states and teleport

--- Ripple Game/Ripple Game/Assets/Scripts/Monster.cs
+++ Ripple Game/Ripple Game/Assets/Scripts/Monster.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour {
+    public static Monster Instance;
     public GameObject player;
     public float speed;
+
+    public enum States {Passive, Toying, Argressive};
+    States monster;
 	// Use this for initialization
 	void Start () {
-		
+        Instance = this;
+        monster = States.Passive;
 	}
 	
 	// Update is called once per frame
@@ -19,5 +24,22 @@ public class Monster : MonoBehaviour {
     {
         float step = speed * Time.deltaTime;
         gameObject.transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+    }
+
+    public void Teleport()
+    {
+        var jumpPos = new Vector3(Random.Range(player.transform.position.x - 50, player.transform.position.x + 50), 
+            0.5f, (Random.Range(player.transform.position.z - 50, 
+            player.transform.position.z + 50)));
+        var dist = Vector3.Distance(player.transform.position, jumpPos);
+        if (dist > 25)
+        {
+            this.gameObject.transform.position = jumpPos;
+        }
+        else
+        {
+            Teleport();
+        }
+        
     }
 }
